386a3743f4f77f773b409db3b4d62bf2540d622a
Hanshul <hanshul.chandel@gmail.com>
2026-08-14 06:57:46 +0530

    Time: 14 ms (30.92%) | Memory: 47.8 MB (21.64%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -3,9 +3,8 @@ class Solution {
         int count = 0, j = 0;
         int[] freq = new int[128];
         for(int i = 0; i<s.length(); i++) {
-            int indx = s.charAt(i);
-            freq[indx]++;
-            while(j<i && freq[indx]>1) {
+            freq[s.charAt(i)]++;
+            while(j<i && freq[s.charAt(i)]>1) {
                 freq[s.charAt(j++)]--;
             }
             count = Math.max(count, i-j+1);
