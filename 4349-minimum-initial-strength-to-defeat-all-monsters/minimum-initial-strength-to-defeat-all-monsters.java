cedbec21e187a6cba7263ec427c8c22ed4458443
Hanshul <hanshul.chandel@gmail.com>
2026-09-08 05:59:58 +0530

    Time: 15 ms (72.65%) | Memory: 161.3 MB (85.04%) - LeetSync

--- 4349-minimum-initial-strength-to-defeat-all-monsters/minimum-initial-strength-to-defeat-all-monsters.java
+++ 4349-minimum-initial-strength-to-defeat-all-monsters/minimum-initial-strength-to-defeat-all-monsters.java
@@ -1,31 +1,33 @@
 class Solution {
     public long minInitialStrength(int[] monsters, int[][] boosts) {
         int n = monsters.length;
-        long[] bonus = new long[n+1];
-        for(int[] i : boosts) {
-            bonus[i[0]]+=i[2];
-            bonus[i[1]+1]-=i[2];
+        long[] boost = new long[n+1];
+        for(int i[] : boosts) {
+            boost[i[0]]+=i[2];
+            boost[i[1]+1]-=i[2];
         }
-        for(int i = 1; i<bonus.length; i++) {
-            bonus[i]+=bonus[i-1];
+        long maxSum = 0;
+        for(int i = 1; i<boost.length; i++) {
+            boost[i]+=boost[i-1];
+            maxSum+=monsters[i-1];
         }
-        long l = 0, r = (long)1e18, strength = -1;
-        while(l<=r) {
-            long mid = l+(r-l)/2;
-            if(isValidStrength(mid, monsters, bonus)) {
-                strength=mid;
-                r=mid-1;
+        long left = 0, right = maxSum, answer = 0;
+        while(left<=right) {
+            long mid = left+(right-left)/2;
+            if(isValidStrength(mid, monsters, boost)) {
+                answer=mid;
+                right=mid-1;
             } else {
-                l=mid+1;
+                left=mid+1;
             }
         }
-        return strength;
+        return answer;
     }
 
     private boolean isValidStrength(long mid, int[] monsters, long[] bonus) {
         for(int i = 0; i<monsters.length; i++) {
             if(mid+bonus[i]>=monsters[i]) {
-                mid=Math.max(0,mid-monsters[i]);
+                mid=Math.max(0L, mid-monsters[i]);
             } else {
                 return false;
             }
