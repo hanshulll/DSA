8a1dfb1e7c6b46261e39273ff74f6b317bd4fe6c
Hanshul <hanshul.chandel@gmail.com>
2026-09-11 07:42:45 +0530

    Time: 1 ms (91.61%) | Memory: 45.3 MB (23.64%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,41 +1,46 @@
 class Solution {
     public int[] findRedundantConnection(int[][] edges) {
-        Dsu union = new Dsu(edges.length+1);
-        for(int i = 0; i<edges.length; i++) {
-            if(!union.union(edges[i][0],edges[i][1])) {
-                return new int[]{edges[i][0],edges[i][1]};
+        Dsu d = new Dsu(edges.length + 1);
+        for (int i = 0; i < edges.length; i++) {
+            // for(int j = 0; j<edges[i].length; j++) {
+            if (d.union(edges[i][0], edges[i][1])) {
+                return new int[] { edges[i][0], edges[i][1] };
             }
+            // }
         }
-        return new int[]{};
+        return new int[] {};
     }
 
     class Dsu {
         int[] parent, size;
+
         public Dsu(int n) {
-            this.parent = new int[n];
-            this.size = new int[n];
-            for(int i = 0; i<n; i++) {
-                parent[i]=i;
-                size[i]=1;
+            parent = new int[n];
+            size = new int[n];
+            for (int i = 0; i < n; i++) {
+                size[i] = 1;
+                parent[i] = i;
             }
         }
 
-            private int findParent(int n) {
-                if(parent[n]==n) return n;
-                return parent[n]=findParent(parent[n]);
-            }
+        public int findParent(int v) {
+            if (parent[v] == v)
+                return v;
+            return parent[v] = findParent(parent[v]);
+        }
 
-            public boolean union(int i, int j) {
-                int rootI = this.findParent(i), rootJ = this.findParent(j);
-                if(rootI==rootJ) return false;
-                if(rootI<rootJ) {
-                    parent[rootI]=rootJ;
-                    size[rootJ]+=size[rootI];
-                } else {
-                    parent[rootJ]=rootI;
-                    size[rootI]+=size[rootJ];
-                }
+        public boolean union(int a, int b) {
+            int aParent = findParent(a), bParent = findParent(b);
+            if (aParent == bParent)
                 return true;
+            if (aParent < bParent) {
+                parent[aParent] = bParent;
+                size[bParent] += size[aParent];
+            } else {
+                parent[bParent] = aParent;
+                size[aParent] += size[bParent];
             }
+            return false;
+        }
     }
 }
